5b4fb903c3f5961b771e7929ba3308c8a455d377
Isahann Hanacleto <isahanacleto@gmail.com>
2023-04-12 23:37:48 -0300

    Creating project

---
+++ src/main/java/com/strbuildtool/StringBuilderToolApplication.java
@@ -0,0 +1,13 @@
+package com.strbuildtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBuilderToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBuilderToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/strbuildtool/StringBuilderToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strbuildtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringBuilderToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
