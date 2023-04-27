82be4f85091102514d916028589d582ce6cdcf0b
Isahann Hanacleto <isahanacleto@gmail.com>
2023-04-27 14:06:08 -0300

    #8 removing project

--- src/test/java/com/strbuildtool/StringBuilderToolApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.strbuildtool;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StringBuilderToolApplicationTests {
-}
